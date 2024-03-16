601b2585edecfd7d9126c42bc1fd9aff7e95e46e
Phil Tyler <laidbackcoder@gmail.com>
2024-03-16 16:38:45 +0000

    Minor Refactoring

--- src/VideoTrim.py
+++ src/VideoTrim.py
@@ -10,8 +10,8 @@ from PIL import Image, ImageTk
 class VideoTrimmerApp(ttk.Window):
     def __init__(self):
         super().__init__(themename='darkly')
-        self.title("Video Trimmer")
-        self.geometry("1000x400")
+        self.title('Video Trimmer: Select a File')
+        self.geometry('1000x400')
 
         # Variables for slider values
         self.start_time = tk.DoubleVar(value=0)
@@ -19,28 +19,28 @@ class VideoTrimmerApp(ttk.Window):
 
         # Button to start encoding
         self.encode_button = tk.Button(
-            self, text="Start Encoding", command=self.trim_video
+            self, text='Start Encoding', command=self.trim_video
         )
         self.encode_button.pack(side=tk.RIGHT, anchor='n')
 
         # Button to choose file
         self.choose_button = tk.Button(
-            self, text="Choose File", command=self.choose_file
+            self, text='Choose File', command=self.choose_file
         )
         self.choose_button.pack(side=tk.RIGHT, anchor='n')
 
         # Frame for start preview
         self.start_frame_group = tk.Frame(self)
-        self.start_frame_group.pack(side="left", padx=50)
+        self.start_frame_group.pack(side='left', padx=50)
 
         # Buttons to adjust start time
         self.start_dec_button = tk.Button(
-            self.start_frame_group, text="<", command=self.decrease_start_time
+            self.start_frame_group, text='<', command=self.decrease_start_time
         )
         self.start_dec_button.grid(row=0, column=0)
 
         self.start_time_label = tk.Label(
-            self.start_frame_group, text="Start: 0:00"
+            self.start_frame_group, text='Start: 0:00'
             )
         self.start_time_label.grid(row=0, column=1)
 
@@ -49,14 +49,14 @@ class VideoTrimmerApp(ttk.Window):
             from_=0,
             to=0,
             length=220,
-            orient="horizontal",
+            orient='horizontal',
             variable=self.start_time,
             command=self.load_previews,
         )
         self.start_slider.grid(row=1, column=0, columnspan=3, pady=10)
 
         self.start_inc_button = tk.Button(
-            self.start_frame_group, text=">", command=self.increase_start_time
+            self.start_frame_group, text='>', command=self.increase_start_time
         )
         self.start_inc_button.grid(row=0, column=2)
 
@@ -65,15 +65,15 @@ class VideoTrimmerApp(ttk.Window):
 
         # Frame for end preview
         self.end_frame_group = tk.Frame(self)
-        self.end_frame_group.pack(side="right", padx=50)
+        self.end_frame_group.pack(side='right', padx=50)
 
         # Buttons to adjust end time
         self.end_dec_button = tk.Button(
-            self.end_frame_group, text="<", command=self.decrease_end_time
+            self.end_frame_group, text='<', command=self.decrease_end_time
         )
         self.end_dec_button.grid(row=0, column=0)
 
-        self.end_time_label = tk.Label(self.end_frame_group, text="End: 0:00")
+        self.end_time_label = tk.Label(self.end_frame_group, text='End: 0:00')
         self.end_time_label.grid(row=0, column=1)
 
         self.end_slider = ttk.Scale(
@@ -81,14 +81,14 @@ class VideoTrimmerApp(ttk.Window):
             from_=0,
             to=0,
             length=220,
-            orient="horizontal",
+            orient='horizontal',
             variable=self.end_time,
             command=self.load_previews,
         )
         self.end_slider.grid(row=1, column=0, columnspan=3, pady=10)
 
         self.end_inc_button = tk.Button(
-            self.end_frame_group, text=">", command=self.increase_end_time
+            self.end_frame_group, text='>', command=self.increase_end_time
         )
         self.end_inc_button.grid(row=0, column=2)
 
@@ -123,6 +123,7 @@ class VideoTrimmerApp(ttk.Window):
             self.start_slider.config(from_=0, to=video_length)
             self.end_slider.config(from_=0, to=video_length)
             self.load_previews()
+            self.title(f'Video Trimmer: "{self.file_path}"')
 
     def load_previews(self):
         start_time_sec = int(self.start_time.get())
@@ -141,7 +142,7 @@ class VideoTrimmerApp(ttk.Window):
             self.preview_label_start.configure(image=frame_start)
             self.preview_label_start.image = frame_start
             self.start_time_label.config(
-                text=f"Start: {start_time_sec//60}:{start_time_sec%60:02d}"
+                text=f'Start: {start_time_sec//60}:{start_time_sec%60:02d}'
             )
 
         self.video_capture_end.set(cv2.CAP_PROP_POS_MSEC, end_time_sec * 1000)
@@ -155,7 +156,7 @@ class VideoTrimmerApp(ttk.Window):
             self.preview_label_end.configure(image=frame_end)
             self.preview_label_end.image = frame_end
             self.end_time_label.config(
-                text=f"End: {end_time_sec//60}:{end_time_sec%60:02d}"
+                text=f'End: {end_time_sec//60}:{end_time_sec%60:02d}'
             )
 
     def decrease_start_time(self):
@@ -204,7 +205,7 @@ class VideoTrimmerApp(ttk.Window):
         )
         subprocess.call(command, shell=True)
 
-        messagebox.showinfo("Success", "Video trimmed successfully!")
+        messagebox.showinfo('Success', 'Video trimmed successfully!')
 
 
 if __name__ == "__main__":
